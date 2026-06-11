64cccbe4c5655384cd1611dd1b3600e2b6ea355a
A-Haseeb1233 <aah05437@gmail.com>
2026-06-11 20:09:55 +0100

    pushing my first lesson of js [Goal Backend Engineer]

---
+++ chapter no 1/variables & data types.js
@@ -0,0 +1,51 @@
+// console.log("FalconH Backend Developer");
+// console.log("apna college");
+// console.log("FalconH") 
+
+// // vairable 
+
+// fullname = "Abdul Haseeb aka FalconH aka Falcon";
+// age = 24;
+// price = 99.99;
+// radius = 5;
+// x = null;
+// y = undefined;
+
+// console.log("fullname: " + fullname);
+// console.log("age: " + age);
+// console.log("price: " + price);
+// console.log("radius: " + radius);
+// console.log(x);
+// console.log(y);
+
+// const student = {
+//     fullName : "Abdul Haseeb",
+//     age : 22,
+//     color : "Black",
+//     cgpa : 2.5
+// };
+
+// student["age"] = student["age"] + 1
+// student["fullName"] = "FalconH"
+// console.log("age: " + student["age"]);
+// console.log("name: " + student.fullName);
+
+
+
+// const product = {
+//     productName = "Ball pen",
+//     rating = 4,
+//     price = 270,
+// };
+
+
+const profile = {
+    fullName : "Shradha Kapra",
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    isFollow : true
+};
+console.log(profile);
+
+console.log(typeof profile["fullName"]);
