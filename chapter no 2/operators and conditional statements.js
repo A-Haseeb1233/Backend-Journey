c54a0f658e125ac364e6751f7d0f05dab390e6d8
A-Haseeb1233 <aah05437@gmail.com>
2026-06-15 13:52:09 +0100

    chapter no is cont....

--- chapter no 2/operators and conditional statements.js
+++ chapter no 2/operators and conditional statements.js
@@ -44,12 +44,55 @@
 
 // assignement operators
 
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b =", b);
+// console.log("a+= : ", a += 2);
+// console.log("a-= : ", a -= 2);
+// console.log("a*= : ", a *= 5);
+// console.log("a /= : ", a /= 5);
+// console.log("a**= : ", a **= 5); // a **= 5 = a = a(5) ** 5
+
+
+// let a = 5;
+// let b = 2;
+
+// let c = a == b;
+
+// console.log(c);
+
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a >= b = ", a >= b); // true a is greater than or equal to  b
+// console.log("a <= b = ", a <= b); // false a is less than or equal to b
+// console.log("a > b = ", a > b); // a is greater than b
+// console.log("a < b = ", a < b); // a is less than b
+// console.log("equal to and not equal to check!");
+// console.log("a == b = ", a == b); // false
+// console.log("a != b = ", a != b); // true
+
+// let x = 5;
+// let y = "5";
+// let z = 5;
+// let m = 4;
+
+// console.log("x == y = ", x == y); // true
+// console.log("x === y = ",  x === y); // false
+// console.log("x != y = ", x != y); // false
+// console.log("x !== y = ", x !== y); // true
+
+// console.log(!((5 == 4) || (4 == 4)));
+
+
 let a = 5;
 let b = 2;
 
-console.log("a = ", a, " & b =", b);
-console.log("a+= : ", a += 2);
-console.log("a-= : ", a -= 2);
-console.log("a*= : ", a *= 5);
-console.log("a /= : ", a /= 5);
-console.log("a**= : ", a **= 5); // a **= 5 = a = a(5) ** 5
+let cond1 = a > b;  // true
+let cond2 = a == "5"  // true
+
+let result = cond1 || cond2; // true
+
+console.log("cond1 && cond2 = ", result);
