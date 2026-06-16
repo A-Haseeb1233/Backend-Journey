1a39c803e538bf5319146f712917122ff33efd33
A-Haseeb1233 <aah05437@gmail.com>
2026-06-16 20:30:52 +0100

    chapter no 3 loops going on

---
+++ chapter no 3/loops and strings.js
@@ -0,0 +1,28 @@
+// for(let i = 1; i < 5; i++){
+//     console.log("The Great Backend Engineer FalconH")
+// }
+
+// let n = Number(prompt("Enter your number: "));
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++){
+//     sum = sum + i;
+// }
+
+// console.log("sum of n numbers is : ", sum);
+
+// for(let i = 1; i <= 100; i++){
+//     console.log("i = ",i);
+// }
+
+// let i = 1;
+// while(i <= 5){
+//     console.log("FalconH");
+//     i++;
+// }
+
+let i = 1;
+do{
+    console.log("Apna college");
+    i++;
+} while (i <= 0);
