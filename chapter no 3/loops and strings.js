2d2dbe9c33450063a4c1db270347ecc84b1c58b4
A-Haseeb1233 <aah05437@gmail.com>
2026-06-17 12:42:30 +0100

    loops complete string initiate

--- chapter no 3/loops and strings.js
+++ chapter no 3/loops and strings.js
@@ -21,8 +21,63 @@
 //     i++;
 // }
 
-let i = 1;
-do{
-    console.log("Apna college");
-    i++;
-} while (i <= 0);
+// let i = 1;
+// do{
+//     console.log("Apna college");
+//     i++;
+// } while (i <= 0);
+
+// let n = Number(prompt("Enter your numbers : "));
+// let start = Number(prompt("Enter your starting range : "));
+// let end = Number(prompt("Enter your Ending range : "));
+// for (let i = start; i <= end; i++){
+//     console.log(n , " * " , i , " = " , i * n)
+//     if(i ===  7){
+//         continue;
+//     }
+// }
+
+// let string = "AbdulHaseeb";
+// let length = 0;
+// for (let i of string){
+//     console.log("i = ",i);
+//     length++;
+// }
+
+// console.log("length of strval is = ", length);
+
+
+// for in loops
+
+// const student = {
+//     fullName : "Abdul Haseeb",
+//     age : 22,
+//     cpga : 2.5,
+//     isPass : true
+// };
+
+// for(let key in student) {
+//     console.log("key = ", key, "values = ", student[key]);
+// }
+
+// return indexes and keys in for in looops
+
+
+// for(let i = 0; i <= 100; i++){
+
+//     if(i % 2 !== 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// let gameNum = 25;
+// let userNum = Number(prompt("Guess a game number: "));
+
+// while(userNum !== gameNum) {
+//     userNum = Number(prompt("you guess a wrong number guess again!"));
+//     console.log("userNum = ", userNum);
+// }
+
+// console.log("Congratulations you entered a right number!");
