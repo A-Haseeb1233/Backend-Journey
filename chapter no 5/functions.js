409da141ad3e668736847a36c892b99060019f5a
A-Haseeb1233 <aah05437@gmail.com>
2026-06-29 22:16:34 +0100

    finally functions are complete

--- chapter no 5/functions.js
+++ chapter no 5/functions.js
@@ -199,16 +199,140 @@
 
 // console.log(numberChecker(78));
 
+// let arr = [2, 3, 4, 5, 6];
+// let funct = (val) => {
+//     console.log(val * val);
+// }
 
 
+// arr.forEach(funct);
+
+// let marks = [56, 90, 98, 67, 96];
 
 
+// let userarr = [];
+
+// for(let i = 0; i <= userarr; i++){
+//     let mark = Number(prompt("Enter your n: "));
+//     userarr.push(mark);
+// }
+
+// function greetuser(name) {
+//     console.log(`Hello, ${name}! Welcome.`);
+// }
+
+// greetuser("Haseeb");
+
+// const greetuser = (name) => {
+//     console.log(`Hello, ${name}! Welcome.`)
+// }
+
+// greetuser("Haseeb");
+
+// const multiplyNumbers = (a, b) => a * b; 
+
+// // another way to right the same thing
+// // const multiplyNumbers = (a, b) => {
+// //     return a * b;    
+// // }
+
+// let result = multiplyNumbers(3, 3);
+// console.log(result);
 
 
+// const numbers = [1, 2, 3, 4, 5];
+
+//     // numbers.forEach(function(val){
+//     //     console.log(val * val);
+//     //     // another method that is we can also return value
+//     // });
+
+// numbers.forEach((val) => {
+//     console.log(val * val);
+// });
+
+// const students = ["Ali", "Sara", "Haseeb", "Zara", "Bilal"];
+
+// students.forEach((name) => {
+//     if(name.length > 4){
+//         console.log(name);
+//     }
+// })
+
+// const students = ["Ali", "Sara", "Haseeb", "Zara", "Bilal"];
+
+// let finalNames = students.filter((name) => {
+//     return name.length > 4;
+// });
+
+// console.log(finalNames);
 
 
+// const numbers = [10, 25, 32, 47, 58, 63, 71, 80];
+
+// let evenarray = numbers.filter((isEven) => isEven % 2 === 0);
+
+// evenarray.forEach((val) => {
+//     console.log(val);
+// });
+
+// const products = [
+//   { name: "Laptop", price: 150000 },
+//   { name: "Mouse", price: 1500 },
+//   { name: "Monitor", price: 45000 },
+//   { name: "Keyboard", price: 3000 },
+//   { name: "Headphones", price: 8000 },
+// ];
+
+// let expensive = products.filter((prices) => prices.price > 5000);
+// expensive.forEach((val) => {
+//     console.log(`${val.name} - ${val.price}`)
+// })
+
+// const students = [
+//   { name: "Ali", marks: 45 },
+//   { name: "Sara", marks: 78 },
+//   { name: "Haseeb", marks: 90 },
+//   { name: "Zara", marks: 33 },
+//   { name: "Bilal", marks: 65 },
+// ];
+
+// let greatermark = students.filter((val) => val.marks > 60);
+// greatermark.forEach((val) => console.log(`${val.name} - ${val.marks}`))
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// let result = numbers.reduce((pre, curr) => {
+//    return  pre + curr;
+
+// })
+
+// console.log(result)
 
 
+// const numbers = [10, 25, 32, 47, 58];
+
+// let max = numbers.reduce((pre, curr) => {
+//     if(pre < curr){
+//         return curr;
+//     } else {
+//         return pre
+//     }
+// }, 0);
+
+// console.log(max)
 
 
+const products = [
+  { name: "Laptop", price: 150000 },
+  { name: "Mouse", price: 1500 },
+  { name: "Monitor", price: 45000 },
+  { name: "Keyboard", price: 3000 },
+  { name: "Headphones", price: 8000 },
+];
 
+let totalPrice = products.reduce((pre, curr) => {
+    return pre + curr.price
+},0)
+
+console.log(totalPrice)
