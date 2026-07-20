f9b22656e6f11a1f0b35deb10b265101ed02bbb3
A-Haseeb1233 <aah05437@gmail.com>
2026-07-21 00:00:33 +0100

    updates

--- chapter no 7/events.js
+++ chapter no 7/events.js
@@ -1,26 +1,54 @@
-let btn = document.querySelector("button");
+// let btn = document.querySelector("button");
 
-// styling
+// const { createElement } = require("react");
 
-btn.style.padding = "20px";
-btn.style.color = "red";
-btn.style.backgroundColor = "black";
-btn.style.border = "none";
+// // styling
 
-// handler
+// btn.style.padding = "20px";
+// btn.style.color = "red";
+// btn.style.backgroundColor = "black";
+// btn.style.border = "none";
+
+// // handler
+
+// btn.addEventListener("click", () => {
+//   console.log("button clicked - Handler1");
+// });
+
+// btn.addEventListener("click", () => {
+//   console.log("button clicked - Handler2");
+// });
+
+// let handler3 = () => {
+//   console.log("button clicked - Handler3");
+// }
+// btn.addEventListener("click", handler3);
+
+// btn.addEventListener("click", () => {
+//   console.log("button clicked - Handler4");
+// });
+
+// btn.removeEventListener("click", handler3)
+
+let btn = document.createElement("button");
+btn.innerText = "Toggle";
+document.querySelector("body").prepend(btn);
+
+// style
+
+btn.style.padding = "10px 30px";
+btn.style.fontFamily = "arial";
+btn.style.fontSize = "20px";
+
+let currMode = "Light";
 
 btn.addEventListener("click", () => {
-  console.log("button clicked - Handler1");
-});
-
-btn.addEventListener("click", () => {
-  console.log("button clicked - Handler2");
-});
-
-btn.addEventListener("click", () => {
-  console.log("button clicked - Handler3");
-});
-
-btn.addEventListener("click", () => {
-  console.log("button clicked - Handler4");gut
+  if (currMode === "Light") {
+    currMode = "Dark";
+    document.body.style.backgroundColor = "black";
+  } else {
+    currMode = "Light";
+    document.body.style.backgroundColor = "white"
+  }
+  console.log(currMode);
 });
