64aef21b4bb922d00d4edcf881e20a9d517049bc
Abdul Haseeb <aah05437@gmail.com>
2026-07-24 03:11:51 +0100

    class and object directory updated

--- chapter no 8/object and classes.js
+++ chapter no 8/object and classes.js
@@ -5,8 +5,30 @@
 // };
 
 
-const employee = {
-    caltax () {
-        console.log("10$")
+// const employee = {
+//     caltax () {
+//         console.log("10$")
+//     }
+// }
+
+
+const tax = {
+    calTax () {
+        console.log(`this is the tax percentage 10`);
     }
+};
+
+const employee1 = {
+    salary : 90000
 }
+
+const employee2 = {
+    salary : 95000
+}
+
+const employee3 = {
+    salary : 92000
+}
+
+employee1.__proto__ = tax;
+Object.setPrototypeOf(employee2, tax);
