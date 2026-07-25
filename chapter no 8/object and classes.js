b43d8c245442d8f89cdc4fdaf209de68876ed335
Abdul Haseeb <aah05437@gmail.com>
2026-07-25 22:14:39 +0100

    updates

--- chapter no 8/object and classes.js
+++ chapter no 8/object and classes.js
@@ -12,23 +12,25 @@
 // }
 
 
-const tax = {
-    calTax () {
-        console.log(`this is the tax percentage 10`);
-    }
-};
+// const tax = {
+//     calTax () {
+//         console.log(`this is the tax percentage 10`);
+//     }
+// };
 
-const employee1 = {
-    salary : 90000
-}
+// const employee1 = {
+//     salary : 90000
+// }
 
-const employee2 = {
-    salary : 95000
-}
+// const employee2 = {
+//     salary : 95000
+// }
 
-const employee3 = {
-    salary : 92000
-}
+// const employee3 = {
+//     salary : 92000
+// }
 
-employee1.__proto__ = tax;
-Object.setPrototypeOf(employee2, tax);
+// employee1.__proto__ = tax;
+// Object.setPrototypeOf(employee2, tax);
+
+
