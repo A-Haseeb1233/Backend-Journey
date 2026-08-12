af8f2f87c03085a462ed706849a0590384da0013
Abdul Haseeb <aah05437@gmail.com>
2026-08-12 17:11:23 +0100

    basic understanding of callback

---
+++ chapter no 9/call-backs.js
@@ -0,0 +1,51 @@
+// setTimeout(() => {
+//     console.log("Hello FalconH")
+// }, 2000);
+
+// console.log("a");
+// console.log("b");
+
+// try {
+//   setTimeout(function () {
+//     console.log("c");
+//   }, 2000);
+// } catch (err) {
+//   console.log(err);
+// }
+
+// console.log("d");
+// console.log("e");
+
+
+// function sum (a, b) {
+//     console.log(a + b);
+// }
+
+// function calculation (a, b, sumCallback){
+//     sumCallback(a, b);
+// }
+
+// calculation(1, 2, sum);
+
+// const sum = (a, b) => console.log(`${a+b}`);
+
+// const calculation = (a, b, sumCallBack) => {
+//     sumCallBack(a, b);
+// }
+
+// calculation(1, 2, (a, b) => {
+//     console.log(`${a + b}`)
+// })
+
+const sayHello = (name) => {
+    console.log(`Hello! ${name}`);
+}
+
+const processUser = (name, callBack) => {
+    sayHello(name);
+    callBack();
+}
+
+processUser("FalconH", () => {
+    console.log(`Welcome! FalconH`);
+})
